9559847e4fdf2e1f8895f5fa30b4e7ab18354c65
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2022-12-27 00:08:33 +0500

    Update Form1.cs

--- T_REX/Game/Form1.cs
+++ T_REX/Game/Form1.cs
@@ -13,20 +13,59 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        
         bool jump = false; // bool is used to check dino is jumping 
         int score = 0; //default score is 0
         int barrierspeed = 10;// set the default speed of barriers 
         int jumpspeed; /// set the default speed of runner 
         int force = 12; // default value of force for jump
         int position;
+        bool GameOver = false;
+        Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            resetGame();/// restart game function
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        
+
+        private void keydown(object sender, KeyEventArgs e)
         {
 
         }
+
+        private void keyup(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void resetGame()
+        {
+            force = 12;
+            jump = false;
+            jumpspeed = 0;
+            score = 0;
+            barrierspeed = 10;
+            Scoretext.Text = "Score" + score;
+            trex.Image = Properties.Resources.dino;
+            GameOver = false;
+            trex.Top = 303;
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "Barriers")
+                {
+
+                }
+            }
+
+        }
+
+        
     }
 }
