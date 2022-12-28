8db8b82d7c22c6a6ba56c368673219d1b2e611e0
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2022-12-28 21:31:44 +0500

    Initial Commit
    Co-Authored-By: Nomiwaleed <120721823+Nomiwaleed@users.noreply.github.com>

--- Game/TREX/Form1.cs
+++ Game/TREX/Form1.cs
@@ -28,6 +28,8 @@ namespace TREX
 
         private void keydown(object sender, KeyEventArgs e)
         {
+            /// if jump is equal to false then player will press space button player move up 
+            // and jump is true
             if (e.KeyCode == Keys.Space && jump == false)
             {
                 jump = true;
@@ -40,7 +42,7 @@ namespace TREX
             {
                 jump = false;
             }
-            if (e.KeyCode == Keys.R && GameOver == true)
+            if (e.KeyCode == Keys.Space && GameOver == true)
             {
                 GameRestart();
             }
@@ -71,7 +73,35 @@ namespace TREX
                 jumpspeed = 0;
 
             }
+            foreach (Control x in Controls)
+            {
+                
+                if (x is PictureBox && (string)x.Tag =="Barriers")
+                {
+                    GameEvent.Stop();
+                    x.Left -= barrierspeed;
+                    if (x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width+random.Next(200,500)+(x.Width*15);
+                        score++;
+                    }
+                    if (trex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        GameEvent.Stop();
+                        trex.Image = Properties.Resources.dead;
+                        Scoretext.Text+= "";
+                        Hiscoretext.Text = "High Score="+score;
+                        MessageBox.Show("Press Space To Restart the Game!");
+                        GameOver = true;
+
+                    }
+                }
+               
+            }
         }
+        /// <summary>
+        /// this function will restart the game 
+        /// </summary>
         private void GameRestart()
         {
             force = 12;
@@ -94,5 +124,9 @@ namespace TREX
             GameEvent.Start();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Game/TREX/Properties/Resources.Designer.cs
+++ Game/TREX/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace TREX.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap obstacle3 {
+            get {
+                object obj = ResourceManager.GetObject("obstacle3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap running {
             get {
                 object obj = ResourceManager.GetObject("running", resourceCulture);
